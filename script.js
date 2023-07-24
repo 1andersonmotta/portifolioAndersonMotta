e2f8363fc3194166f5d6ef55e30ec83b543183c8
Anderson Motta <anderson.motta@tegra.com.br>
2023-07-24 13:41:13 -0300

    fix add animation

--- script.js
+++ script.js
@@ -296,6 +296,32 @@ gsap.fromTo(".footer_sociais li", {
 })
 
 
+function random() {
+    gsap.to(".diferente_n", {
+        textShadow: `1px 1px 1px ${getRandomColor()},0 0 1vw ${getRandomColor()},0 0 1vw ${getRandomColor()}, 0 0 1vw ${getRandomColor()},0 0 1vw ${getRandomColor()}`,
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+    })
+    gsap.to(".img", {
+        border: `2px solid ${getRandomColor()}`,
+        duration: 1,
+        repeat: -1,
+        yoyo: true,
+        ease: "power1.inOut",
+    })
+}
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+setInterval(random, 3000)
 
 
 
